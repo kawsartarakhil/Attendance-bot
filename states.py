54b1f165525b57c31f6e9d25be0c693c7f14e876
kawsartarakhil <kawsartarakhil9@gmail.com>
2026-09-03 05:26:32 +0500

    added states

---
+++ states.py
@@ -0,0 +1,25 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class CreateGroupStates(StatesGroup):
+    group_name=State()
+    course=State()
+    teacher=State()
+    room=State()
+    confirm=State()
+
+
+class CreateScheduleStates(StatesGroup):
+    group=State()
+    weekday=State()
+    start_time=State()
+    end_time=State()
+    room=State()
+    confirm=State()
+
+
+class ManualAttendanceStates(StatesGroup):
+    student=State()
+    status=State()
+    reason=State()
+    confirm=State()
